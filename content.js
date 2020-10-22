8e33b8241775d99391cf48e72b91b03475a52001
Kieth <fallenwings@protonmail.com>
2020-10-23 05:25:30 +0800

    Added presentur

--- content.js
+++ content.js
@@ -2,8 +2,13 @@
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
     console.log('at content');
-    const elem = document.getElementById('playback-video-playback-video_html5_api');
     if (msg.action && msg.action === "onIconClicked"){
+        const id_list = ['playback-video-playback-video_html5_api', 'Video1_html5_api'];
+        let elem;
+        for (const id of id_list){
+            elem = document.getElementById(id);
+            if (!!elem) break;
+        }
         if (elem) sendResponse(elem.src);
         sendResponse(elem);
     }    
